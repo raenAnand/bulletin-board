ffc58e5555555fca1e883beb63193a4b6c706d9a
Deepak Adhikari <deeps.adhi@gmail.com>
2017-07-06 06:28:44 +0545

    Add jwplayer config

--- config.example.js
+++ config.example.js
@@ -1,6 +1,10 @@
 // Configuration
 var _CONFIG = {
   apiUrl: '',
+  jwplayer: {
+    jsFile: '',
+    url: 'https://content.jwplatform.com/libraries/'
+  },
   googleAnalyticsCode: '',
   fetchInterval: 60 * 1000
 };
