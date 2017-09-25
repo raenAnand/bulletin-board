b8b0824798f56b42cfd58d34a60aec335219b6e7
Deepak Adhikari <deeps.adhi@gmail.com>
2017-09-25 10:53:18 +0545

    Hide change to opacity

--- js/main.js
+++ js/main.js
@@ -52,9 +52,11 @@
 
     cachedTitle = segement.title;
     webview[hidden].src = segement.url;
-    webview[hidden].style.display = 'none';
+    // webview[hidden].style.display = 'none';
+    webview[hidden].style.opacity = 0;
 
-    webview[shown].style.display = 'block';
+    // webview[shown].style.display = 'block';
+    webview[shown].style.opacity = 100;
 
     shown = (shown) ? 0 : 1;
     hidden = (hidden) ? 0 : 1;
