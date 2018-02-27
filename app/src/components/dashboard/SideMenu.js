d1ceb9b9ffbe05bec549d40a75a27d03e9ca5e78
aanchal <aanchal.adk@gmail.com>
2018-02-27 07:57:37 +0545

    add react-fontawesome and use it in the sidepanel

--- app/src/components/dashboard/SideMenu.js
+++ app/src/components/dashboard/SideMenu.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
+import FontAwesome from 'react-fontawesome';
 
 class SideMenu extends Component {
 
-  render () {
+  render() {
     return (
       <div>
         <h3>Bulletin Dashboard</h3>
         <ul>
-          <li><a href="#">Add</a></li>
-          <li><a href="#">List</a></li>
+          <li>
+            <a href="#" className="clearfix">
+              <span>Add</span>
+              <FontAwesome
+                className="super-crazy-colors"
+                name="plus-square"
+                size="2x"
+                style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+              />
+            </a>
+          </li>
+          <li>
+            <a href="#" className="clearfix">
+              <span>List</span>
+              <FontAwesome
+                className="super-crazy-colors"
+                name="list-alt"
+                size="2x"
+                style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+              />
+            </a>
+          </li>
         </ul>
       </div>
     );
